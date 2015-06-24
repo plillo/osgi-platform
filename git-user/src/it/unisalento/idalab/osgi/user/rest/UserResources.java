ff2d6be994577132f3fdf7ffc20d8e373c92923d
Paolo Lillo <paolo.lillo@hash.it>
2015-06-24 22:55:32 +0200

    clean warnings

--- git-user/src/it/unisalento/idalab/osgi/user/rest/UserResources.java
+++ git-user/src/it/unisalento/idalab/osgi/user/rest/UserResources.java
@@ -1,6 +1,5 @@
 package it.unisalento.idalab.osgi.user.rest;
 
-import java.net.HttpURLConnection;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -15,9 +14,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.amdatu.web.rest.doc.Description;
 import org.amdatu.web.rest.doc.Notes;
