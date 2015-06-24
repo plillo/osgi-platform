4d4d4a68ad8c87428e907a034237aa07b0d2421e
pierorametta <Piero@Piero-HP>
2015-06-24 11:57:15 +0200

    aggiunta pulsante register with facebook

--- git-user/static/userUI/js/controllers.js
+++ git-user/static/userUI/js/controllers.js
@@ -28,11 +28,21 @@ angular.module("userUI").controller('RegisterController', ['$scope', '$http', '$
 	}
     
     $scope.googleRegistration = function() {
-    	//alert("Google Redirect");
-    	$window.location.href='https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri=http://localhost:8080/Google/Oauth2callback&response_type=code&client_id=553003668237-olh0snp5lfpl6k6h4rophl6on5u7fodd.apps.googleusercontent.com&approval_prompt=force';
-    	
+    	$window.location.href='https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri=http://localhost:8080/Oauth/Oauth2callback&response_type=code&client_id=553003668237-olh0snp5lfpl6k6h4rophl6on5u7fodd.apps.googleusercontent.com&approval_prompt=force&state=google';
+    	//$window.open('https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri=http://localhost:8080/Oauth/Oauth2callback&response_type=code&client_id=553003668237-olh0snp5lfpl6k6h4rophl6on5u7fodd.apps.googleusercontent.com&approval_prompt=force&state=google');
+    	//$http.jsonp('https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri=http://localhost:8080/Google/Oauth2callback&response_type=code&client_id=553003668237-olh0snp5lfpl6k6h4rophl6on5u7fodd.apps.googleusercontent.com&approval_prompt=force?callback=JSON_CALLBACK"');
+    	//.success(function(){alert('ok')});
+    }
+    
+    $scope.facebookRegistration = function() {
+    	$window.location.href='https://www.facebook.com/dialog/oauth?scope=email&redirect_uri=http://localhost:8080/Oauth/Oauth2callback&response_type=code&client_id=492601017561308&approval_prompt=force&state=facebook';
+    	//$window.open('https://www.facebook.com/dialog/oauth?scope=email&redirect_uri=http://localhost:8080/Oauth/Oauth2callback&response_type=code&client_id=492601017561308&approval_prompt=force&state=facebook');
     }
 
+    function JSON_CALLBACK(data) {
+    	alert('ok');
+    }
+    
 	$scope.validate = function() {
 		if($scope.newUser.password != $scope.repeatPassword) {
 			return false;
