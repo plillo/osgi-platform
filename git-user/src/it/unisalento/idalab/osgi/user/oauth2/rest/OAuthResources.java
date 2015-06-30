ca27e2608ee14e754fa463bba25eef1ea6b75c82
vinx <vincenzo.mighali@unisalento.it>
2015-06-30 18:32:59 +0200

    - parametro URL "state" passato come JSON - parametri e URL per OAuth2 presi da file cfg

--- git-user/src/it/unisalento/idalab/osgi/user/oauth2/rest/OAuthResources.java
+++ git-user/src/it/unisalento/idalab/osgi/user/oauth2/rest/OAuthResources.java
@@ -1,5 +1,6 @@
 package it.unisalento.idalab.osgi.user.oauth2.rest;
 
+import java.io.IOException;
 import java.util.Map;
 
 import it.unisalento.idalab.osgi.user.api.User;
@@ -13,6 +14,9 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.amdatu.web.rest.doc.Description;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 @Path("OAuth2")
 @Description("API for OAuth management version 1.0")
@@ -25,13 +29,32 @@ public class OAuthResources {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Description("OAuth callback")
 	public Map<String, Object> callback(@QueryParam("code") String code, @QueryParam("state") String state) {
-		Map<String, Object> authenticated_user_map = _OAuthManager.authenticate(code, state);
 		
-        // Create user
-        User user = new User();
-        user.setEmail((String)authenticated_user_map.get("email"));
-        user.setFirstName((String)authenticated_user_map.get("firstName"));
-        user.setLastName((String)authenticated_user_map.get("lastName"));
-        return _userService.createUser(user);
+		try {
+			// Get authentication source
+			ObjectMapper mapper = new ObjectMapper();
+	        JsonNode jsn;	
+			jsn = mapper.readTree(state);
+	        String authenticator = jsn.path("authenticator").getTextValue();
+	        
+			Map<String, Object> authenticated_user_map = _OAuthManager.authenticate(code, authenticator);
+			
+	        // Create user
+	        User user = new User();
+	        user.setEmail((String)authenticated_user_map.get("email"));
+	        user.setFirstName((String)authenticated_user_map.get("firstName"));
+	        user.setLastName((String)authenticated_user_map.get("lastName"));
+	        
+	        return _userService.createUser(user);
+	        
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// TODO: chiedere a Paolo verifica "return null"
+        return null;
 	}
 }
