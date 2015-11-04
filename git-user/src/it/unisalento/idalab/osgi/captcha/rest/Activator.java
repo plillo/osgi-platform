819fd28e6e0ff250fabb66deee09acf8b9b4c774
Paolo Lillo <paolo.lillo@hash.it>
2015-11-04 08:38:59 +0100

    xx

--- git-user/src/it/unisalento/idalab/osgi/captcha/rest/Activator.java
+++ git-user/src/it/unisalento/idalab/osgi/captcha/rest/Activator.java
@@ -5,7 +5,7 @@ import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
 import org.osgi.framework.BundleContext;
 
-
+// Activator
 public class Activator extends DependencyActivatorBase {
 	@Override
 	public void init(BundleContext context, DependencyManager dm) 
