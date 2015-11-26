45dd3af14fe961710b3ac13bd6f56ad8ca466676
Paolo Lillo <paolo.lillo@hash.it>
2015-11-26 11:18:10 +0100

    load

--- aws-study/src/it/hash/osgi/aws/console/ShellCommands.java
+++ aws-study/src/it/hash/osgi/aws/console/ShellCommands.java
@@ -2,10 +2,13 @@ package it.hash.osgi.aws.console;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
@@ -21,6 +24,19 @@ import com.amazonaws.services.simpledb.model.DomainMetadataRequest;
 import com.amazonaws.services.simpledb.model.DomainMetadataResult;
 import com.amazonaws.services.simpledb.model.ListDomainsRequest;
 import com.amazonaws.services.simpledb.model.ListDomainsResult;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClient;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.DeleteMessageRequest;
+import com.amazonaws.services.sqs.model.DeleteQueueRequest;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
 
 public class ShellCommands {
 	public volatile Console console;
@@ -147,6 +163,114 @@ public class ShellCommands {
                 System.out.println("Reponse Status Code: " + ase.getStatusCode());
                 System.out.println("Error Code: " + ase.getErrorCode());
                 System.out.println("Request ID: " + ase.getRequestId());
+        }
+	}
+	
+	public void ses() {
+	    final String FROM = "paolo.lillo@hash.it";  // Replace with your "From" address. This address must be verified.
+	    final String TO = "paolo.lillo@unisalento.it"; // Replace with a "To" address. If you have not yet requested
+	                                                   // production access, this address must be verified.
+	    final String BODY = "This email was sent through Amazon SES by using the AWS SDK for Java.";
+	    final String SUBJECT = "Amazon SES test (AWS SDK for Java)";
+		
+        // Construct an object to contain the recipient address.
+        Destination destination = new Destination().withToAddresses(new String[]{TO});
+
+        // Create the subject and body of the message.
+        Content subject = new Content().withData(SUBJECT);
+        Content textBody = new Content().withData(BODY);
+        Body body = new Body().withText(textBody);
+
+        // Create a message with the specified subject and body.
+        Message message = new Message().withSubject(subject).withBody(body);
+
+        // Assemble the email.
+        SendEmailRequest request = new SendEmailRequest().withSource(FROM).withDestination(destination).withMessage(message);
+
+        try {
+            System.out.println("Attempting to send an email through Amazon SES by using the AWS SDK for Java...");
+
+            // Instantiate an Amazon SES client, which will make the service call with the supplied AWS credentials.
+            AmazonSimpleEmailServiceClient client = new AmazonSimpleEmailServiceClient(console.getCredentials());
+
+            Region REGION = Region.getRegion(Regions.EU_WEST_1);
+            client.setRegion(REGION);
+
+            // Send the email.
+            client.sendEmail(request);
+            System.out.println("Email sent!");
+
+        } catch (Exception ex) {
+            System.out.println("The email was not sent.");
+            System.out.println("Error message: " + ex.getMessage());
+        }
+	}
+	
+	public void sqs(){
+        AmazonSQS sqs = new AmazonSQSClient(console.getCredentials());
+        Region usWest2 = Region.getRegion(Regions.US_WEST_2);
+        sqs.setRegion(usWest2);
+
+        System.out.println("===========================================");
+        System.out.println("Getting Started with Amazon SQS");
+        System.out.println("===========================================\n");
+
+        try {
+            // Create a queue
+            System.out.println("Creating a new SQS queue called MyQueue.\n");
+            CreateQueueRequest createQueueRequest = new CreateQueueRequest("MyQueue");
+            String myQueueUrl = sqs.createQueue(createQueueRequest).getQueueUrl();
+
+            // List queues
+            System.out.println("Listing all queues in your account.\n");
+            for (String queueUrl : sqs.listQueues().getQueueUrls()) {
+                System.out.println("  QueueUrl: " + queueUrl);
+            }
+            System.out.println();
+
+            // Send a message
+            System.out.println("Sending a message to MyQueue.\n");
+            sqs.sendMessage(new SendMessageRequest(myQueueUrl, "This is my message text."));
+
+            // Receive messages
+            System.out.println("Receiving messages from MyQueue.\n");
+            ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(myQueueUrl);
+            List<com.amazonaws.services.sqs.model.Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
+            for (com.amazonaws.services.sqs.model.Message message : messages) {
+                System.out.println("  Message");
+                System.out.println("    MessageId:     " + message.getMessageId());
+                System.out.println("    ReceiptHandle: " + message.getReceiptHandle());
+                System.out.println("    MD5OfBody:     " + message.getMD5OfBody());
+                System.out.println("    Body:          " + message.getBody());
+                for (Entry<String, String> entry : message.getAttributes().entrySet()) {
+                    System.out.println("  Attribute");
+                    System.out.println("    Name:  " + entry.getKey());
+                    System.out.println("    Value: " + entry.getValue());
+                }
+            }
+            System.out.println();
+
+            // Delete a message
+            System.out.println("Deleting a message.\n");
+            String messageRecieptHandle = messages.get(0).getReceiptHandle();
+            sqs.deleteMessage(new DeleteMessageRequest(myQueueUrl, messageRecieptHandle));
+
+            // Delete a queue
+            System.out.println("Deleting the test queue.\n");
+            sqs.deleteQueue(new DeleteQueueRequest(myQueueUrl));
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which means your request made it " +
+                    "to Amazon SQS, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which means the client encountered " +
+                    "a serious internal problem while trying to communicate with SQS, such as not " +
+                    "being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
         }
 	}
 }
