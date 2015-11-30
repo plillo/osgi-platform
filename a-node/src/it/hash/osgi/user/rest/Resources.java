96e33a113c3e9d5138c48b43554dca89a76dbf57
Paolo Lillo <paolo.lillo@hash.it>
2015-11-30 15:46:27 +0100

    localrepo

--- a-node/src/it/hash/osgi/user/rest/Resources.java
+++ a-node/src/it/hash/osgi/user/rest/Resources.java
@@ -1,5 +1,20 @@
 package it.hash.osgi.user.rest;
 
-public class Resources {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import it.hash.osgi.user.service.UserService;
+
+@Path("users/1.0")
+public class Resources {
+	private volatile UserService _userService;
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response list() {
+		return Response.ok().header("Access-Control-Allow-Origin", "*").entity(_userService.getUsers()).build();
+	}
 }
