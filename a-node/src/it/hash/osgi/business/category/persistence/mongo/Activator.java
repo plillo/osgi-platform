76a70fdb7238b5efffdb47be6f3389abbabb7d15
Paolo Lillo <paolo.lillo@hash.it>
2016-01-15 19:55:41 +0100

    categories

---
+++ a-node/src/it/hash/osgi/business/category/persistence/mongo/Activator.java
@@ -0,0 +1,30 @@
+package it.hash.osgi.business.category.persistence.mongo;
+
+import java.util.Properties;
+
+import org.amdatu.mongo.MongoDBService;
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+
+import it.hash.osgi.business.category.persistence.api.CategoryPersistence;
+
+public class Activator extends DependencyActivatorBase {
+    @Override
+    public synchronized void init(BundleContext context, DependencyManager manager) throws Exception {
+    	Properties props = new Properties();
+		props.put(Constants.SERVICE_RANKING, 2);
+    	manager.add(createComponent()
+        	.setInterface(CategoryPersistence.class.getName(), props)
+            .setImplementation(CategoryPersistenceImpl.class)
+            .add(createServiceDependency()
+                .setService(MongoDBService.class)
+                .setRequired(true))
+                );
+    }
+
+    @Override
+    public synchronized void destroy(BundleContext context, DependencyManager manager) throws Exception {
+    }
+}
---
+++ a-node/src/it/hash/osgi/business/category/shell/Activator.java
@@ -0,0 +1,31 @@
+package it.hash.osgi.business.category.shell;
+
+import java.util.Properties;
+
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
+import org.apache.felix.service.command.CommandProcessor;
+import org.osgi.framework.BundleContext;
+
+import it.hash.osgi.business.category.service.CategoryService;
+
+public class Activator extends DependencyActivatorBase {
+
+	@Override
+	public void init(BundleContext context, DependencyManager manager) throws Exception {
+    	Properties props = new Properties();
+		props.put(CommandProcessor.COMMAND_SCOPE, "category");
+		props.put(CommandProcessor.COMMAND_FUNCTION, new String[] {"addCategory"});
+		manager.add(createComponent()
+				.setInterface(Object.class.getName(), props)
+				.setImplementation(Commands.class)
+				.add(createServiceDependency().setService(CategoryService.class).setRequired(true)));
+		
+		System.out.println("Business category shell actived");
+	}
+
+	@Override
+	public synchronized void destroy(BundleContext context, DependencyManager manager) throws Exception {
+	}
+
+}
